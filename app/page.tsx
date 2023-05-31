0ed9e33fd4796e0c0542065ad9d2875b54e549e7
Mark Gardner <themarkgardner@gmail.com>
2023-05-30 22:25:21 -0500

    added pricing component

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,44 @@
-
 import Header from './components/Header';
 import RestaurantCard from './components/ResturantCard';
+import { Cuisine, Location, PRICE, PrismaClient } from '@prisma/client';
 
-export default function Home() {
+export interface RestaurantCardType {
+  id:number;
+  name:string;
+  main_image:string;
+  cuisine: Cuisine;
+  location: Location;
+  price: PRICE;
+  slug: string;
+}
 
+const prisma = new PrismaClient();
+
+const fetchRestaurants = async (): Promise <RestaurantCardType[]> => {const restaurants = await prisma.restaurant.findMany({
+  select: {
+    id:true,
+    name:true,
+    main_image:true,
+    cuisine:true,
+    slug: true,
+    location:true,
+    price:true,
+  }
+});
+  return restaurants;
+};
+
+export default async function Home() {
+  const restaurants = await fetchRestaurants();
   return (
 
         <main>
           <Header />
           <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
-            <RestaurantCard />
+            {restaurants.map((restaurant) => (
+            <RestaurantCard restaurant={restaurant}/>
+            ))}
           </div>
-        </main>
-   
+        </main> 
   )
 }
